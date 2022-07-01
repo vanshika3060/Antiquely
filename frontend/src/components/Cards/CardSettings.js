87516b93027e7e26b4b12729ebaa5a13f3a94cd2
Bharatwaaj Shankaranarayanan <bharatwaaj.1996@gmail.com>
2022-07-01 20:56:52 -0300

    Added Few Steps

--- frontend/src/components/Cards/CardSettings.js
+++ frontend/src/components/Cards/CardSettings.js
@@ -3,15 +3,16 @@ import React from "react";
 // components
 
 export default function CardSettings() {
-  
   // TODO: Vanshika the form
-  
+
   return (
     <>
       <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
         <div className="rounded-t bg-white mb-0 px-6 py-6">
           <div className="text-center flex justify-between">
-            <h6 className="text-blueGray-700 text-xl font-bold">Sell an Antique Product</h6>
+            <h6 className="text-blueGray-700 text-xl font-bold">
+              Sell an Antique Product
+            </h6>
             <button
               className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
               type="button"
@@ -25,7 +26,31 @@ export default function CardSettings() {
             <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
               Item Information
             </h6>
+            
             <div className="flex flex-wrap">
+            <div className="w-full lg:w-6/12 px-4">
+              <div className="relative w-full mb-3">
+                <label
+                  className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                  htmlFor="grid-password"
+                >
+                  Antique Picture
+                </label>
+                <button
+                  className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                  type="button"
+                >
+                  <i className="fas fa-upload"></i> Upload
+                </button>
+              </div>
+            </div>
+            <div className="w-6/12 sm:w-4/12 px-4">
+              <img
+                src={require("assets/img/jug.jpeg").default}
+                alt="..."
+                className="shadow-lg rounded-full max-w-full h-auto align-middle border-none"
+              />
+            </div>
               <div className="w-full lg:w-6/12 px-4">
                 <div className="relative w-full mb-3">
                   <label
@@ -41,7 +66,7 @@ export default function CardSettings() {
                   />
                 </div>
               </div>
-              
+
               <div className="w-full lg:w-6/12 px-4">
                 <div className="relative w-full mb-3">
                   <label
@@ -80,37 +105,33 @@ export default function CardSettings() {
               About Auction Time
             </h6>
             <div className="w-full lg:w-6/12 px-4">
-                <div className="relative w-full mb-3">
-                  <label
-                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                    htmlFor="grid-password"
-                  >
-                    Auction start time:
-                  </label>
-                  <input
-                    type="datetime-local"
-                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    
-                  />
-                </div>
+              <div className="relative w-full mb-3">
+                <label
+                  className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                  htmlFor="grid-password"
+                >
+                  Auction start time:
+                </label>
+                <input
+                  type="datetime-local"
+                  className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                />
               </div>
-              <div className="w-full lg:w-6/12 px-4">
-                <div className="relative w-full mb-3">
-                  <label
-                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                    htmlFor="grid-password"
-                  >
-                    Auction end time:
-                  </label>
-                  <input
-                    type="datetime-local"
-                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    
-                  />
-                </div>
+            </div>
+            <div className="w-full lg:w-6/12 px-4">
+              <div className="relative w-full mb-3">
+                <label
+                  className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                  htmlFor="grid-password"
+                >
+                  Auction end time:
+                </label>
+                <input
+                  type="datetime-local"
+                  className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                />
               </div>
-           
-              
+            </div>
 
             <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
               About Item
@@ -127,7 +148,7 @@ export default function CardSettings() {
                   <textarea
                     type="text"
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                   // defaultValue="Tell users about the product."
+                    // defaultValue="Tell users about the product."
                     rows="4"
                   ></textarea>
                 </div>
