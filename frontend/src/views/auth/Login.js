76b7230796016bd9cf1a8053bd2aa21fd68653af
Bharatwaaj Shankaranarayanan <bharatwaaj.1996@gmail.com>
2022-07-01 13:47:10 -0300

    Added Vanshika's Tasks

--- frontend/src/views/auth/Login.js
+++ frontend/src/views/auth/Login.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 export default function Login() {
   return (
     <>
+      {/* TODO: @Vanshika Fix this content to align with Antiquely */}
       <div className="container mx-auto px-4 h-full">
         <div className="flex content-center items-center justify-center h-full">
           <div className="w-full lg:w-4/12 px-4">
--- frontend/src/views/auth/Register.js
+++ frontend/src/views/auth/Register.js
@@ -3,6 +3,7 @@ import React from "react";
 export default function Register() {
   return (
     <>
+      {/* TODO: @Vanshika Fix this content to align with Antiquely */}
       <div className="container mx-auto px-4 h-full">
         <div className="flex content-center items-center justify-center h-full">
           <div className="w-full lg:w-6/12 px-4">
