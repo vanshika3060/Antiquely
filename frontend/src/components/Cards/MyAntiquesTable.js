943345ed9b1046dd58c3dd6498bdbfd69c1d0239
Bharatwaaj Shankaranarayanan <bharatwaaj.1996@gmail.com>
2022-07-19 13:21:46 -0300

    Modified Code and Frontend Integration

---
+++ frontend/src/components/Cards/MyAntiquesTable.js
@@ -0,0 +1,176 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+// components
+
+import TableDropdown from "components/Dropdowns/TableDropdown.js";
+
+export default function MyAntiquesTable({ title, color, products }) {
+
+  return (
+    <>
+    <div
+      className={
+        "relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded " +
+        (color === "light" ? "bg-white" : "bg-lightBlue-900 text-white")
+      }
+    >
+      <div className="rounded-t mb-0 px-4 py-3 border-0">
+        <div className="flex flex-wrap items-center">
+          <div className="relative w-full px-4 max-w-full flex-grow flex-1">
+            <h3
+              className={
+                "font-semibold text-lg " +
+                (color === "light" ? "text-blueGray-700" : "text-white")
+              }
+            >
+              {title}
+            </h3>
+          </div>
+        </div>
+      </div>
+      <div className="block w-full overflow-x-auto">
+        {/* Projects table */}
+        <table className="items-center w-full bg-transparent border-collapse">
+          <thead>
+            <tr>
+              <th
+                className={
+                  "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                  (color === "light"
+                    ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                    : "bg-lightBlue-800 text-lightBlue-300 border-lightBlue-700")
+                }
+              >
+                Product
+              </th>
+              <th
+                className={
+                  "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                  (color === "light"
+                    ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                    : "bg-lightBlue-800 text-lightBlue-300 border-lightBlue-700")
+                }
+              >
+                Min Bid Amount
+              </th>
+              <th
+                className={
+                  "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                  (color === "light"
+                    ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                    : "bg-lightBlue-800 text-lightBlue-300 border-lightBlue-700")
+                }
+              >
+                Product Description
+              </th>
+              <th
+                className={
+                  "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                  (color === "light"
+                    ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                    : "bg-lightBlue-800 text-lightBlue-300 border-lightBlue-700")
+                }
+              >
+                Start Date
+              </th>
+              <th
+                className={
+                  "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                  (color === "light"
+                    ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                    : "bg-lightBlue-800 text-lightBlue-300 border-lightBlue-700")
+                }
+              >
+                End Date
+              </th>
+              {/* <th
+                className={
+                  "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                  (color === "light"
+                    ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                    : "bg-lightBlue-800 text-lightBlue-300 border-lightBlue-700")
+                }
+              >
+                Bidders
+              </th> */}
+            </tr>
+          </thead>
+          <tbody>
+            {products &&
+              products.length > 0 &&
+              products.map((product) => (
+                <tr>
+                  <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
+                    <img
+                      src={product.product_image_url}
+                      className="h-12 w-12 bg-white rounded-full border"
+                      alt="..."
+                    ></img>{" "}
+                    <span
+                      className={
+                        "ml-3 font-bold " +
+                        +(color === "light"
+                          ? "text-blueGray-600"
+                          : "text-white")
+                      }
+                    >
+                      {product.product_name}
+                    </span>
+                  </th>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    {product.product_minimum_bid_amount + " CAD"}
+                  </td>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    {product.product_description}
+                  </td>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    {new Date(product.product_auction_start_date_time).toDateString()}
+                  </td>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    {new Date(product.product_auction_end_date_time).toDateString()}
+                  </td>
+                  {/* <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    <div className="flex">
+                      <img
+                        src={require("assets/img/team-1-800x800.jpg").default}
+                        alt="..."
+                        className="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow"
+                      ></img>
+                      <img
+                        src={require("assets/img/team-2-800x800.jpg").default}
+                        alt="..."
+                        className="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow -ml-4"
+                      ></img>
+                      <img
+                        src={require("assets/img/team-3-800x800.jpg").default}
+                        alt="..."
+                        className="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow -ml-4"
+                      ></img>
+                      <img
+                        src={require("assets/img/team-4-470x470.png").default}
+                        alt="..."
+                        className="w-10 h-10 rounded-full border-2 border-blueGray-50 shadow -ml-4"
+                      ></img>
+                    </div>
+                  </td> */}
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
+                    <TableDropdown />
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </>
+  );
+}
+
+MyAntiquesTable.defaultProps = {
+  color: "light",
+};
+
+MyAntiquesTable.propTypes = {
+  color: PropTypes.oneOf(["light", "dark"]),
+};
